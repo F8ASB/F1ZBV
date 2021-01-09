1b7fc8a3f0380bb0abb0a9926e1ebffc8de8aca8
F8ASB <f8asb@hotmail.fr>
2021-01-09 11:27:29 +0100

    Update internet.sh

--- internet.sh
+++ internet.sh
@@ -5,8 +5,8 @@ if : >/dev/tcp/8.8.8.8/53; then
   echo 'Internet available.'
 else
   echo 'Offline.'
-  echo 1 > /sys/class/gpio/gpio20/value
+  echo 1 > /sys/class/gpio/gpio2/value
   echo "Reboot web [`date`]" >> /var/log/netcheck
   sleep 10
-  echo 0 > /sys/class/gpio/gpio20/value
+  echo 0 > /sys/class/gpio/gpio2/value
 fi
